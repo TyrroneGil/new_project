d20635ee117a83c739dc52c9264da97a4aba7ede
TyrroneGil <130520784+TyrroneGil@users.noreply.github.com>
2026-03-26 10:48:30 +0800

    Added DRF and Serializer

--- website_app/urls.py
+++ website_app/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from website_app import views
-urlpatterns = [
-    path('',views.renderIndex),
-    path('login',views.renderLogin),
-    path('home',views.renderHomePage),
-    path('addUser',views.AddUserWithHash),
-    path('auth',views.Authenticate),
-    path('addTodo', views.addTodo),
-    path('getTodo',views.readTodo),
-    path('deleteTodo/<int:id>',views.deleteTodo)
-]
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSets
+
+router = DefaultRouter()
+router.register('users',UserViewSets)
+urlpatterns = router.urls
--- website_app/views.py
+++ website_app/views.py
@@ -4,74 +4,98 @@ from django.contrib.auth.hashers import make_password, check_password
 from .models import ApiDetails, TodoList, Users
 from django.views.decorators.csrf import csrf_exempt
 import json
+from rest_framework import  viewsets
+from .serializer import UserModelSerializer
 
-# Create your views here.
-def renderIndex(request):
-    details = list(ApiDetails.objects.all().values())
-    return render(request,'index.html')
 
-def renderLogin(request):
-    return render(request,'login.html')    
+class UserViewSets(viewsets.ModelViewSet):
+    queryset=Users.objects.all()
+    serializer_class=UserModelSerializer
 
-def renderHomePage(request):
-    return render(request,'homepage.html')
-@csrf_exempt
-def fetchFromForm(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
 
-        username = data.get("username")
-        password = data.get("password")
-    return JsonResponse({"username":username,"password":password})
 
-# def updateData(request):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Create your views here.
+# def renderIndex(request):
+#     details = list(ApiDetails.objects.all().values())
+#     return render(request,'index.html')
+
+# def renderLogin(request):
+#     return render(request,'login.html')    
+
+# def renderHomePage(request):
+#     return render(request,'homepage.html')
+# @csrf_exempt
+# def fetchFromForm(request):
 #     if request.method == "POST":
-#         uid = request.POST['id']
-#         Users.objects.get(id=uid)
+#         data = json.loads(request.body)
+
+#         username = data.get("username")
+#         password = data.get("password")
+#     return JsonResponse({"username":username,"password":password})
+
+# # def updateData(request):
+# #     if request.method == "POST":
+# #         uid = request.POST['id']
+# #         Users.objects.get(id=uid)
 
 
-def AddUserWithHash(request):
-    if request.method == "POST":
-        username=request.POST['username']
-        password = request.POST['password']
-        hashedPassword = make_password(password)
-        user = Users.objects.create(
-            username=username,
-            password=hashedPassword
-        )
-    return redirect('/login')
+# def AddUserWithHash(request):
+#     if request.method == "POST":
+#         username=request.POST['username']
+#         password = request.POST['password']
+#         hashedPassword = make_password(password)
+#         user = Users.objects.create(
+#             username=username,
+#             password=hashedPassword
+#         )
+#     return redirect('/login')
 
 
-def Authenticate(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = Users.objects.get(username=username)
-        if check_password(password,user.password):
-            return redirect('/home')
-        else:
-            return redirect('/login')
+# def Authenticate(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = Users.objects.get(username=username)
+#         if check_password(password,user.password):
+#             return redirect('/home')
+#         else:
+#             return redirect('/login')
 
-@csrf_exempt
-def addTodo(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        title = data.get("title")
-        desc = data.get('description')
-        status = data.get('status')
-        TodoList.objects.create(title=title,description=desc)
-        return JsonResponse({"message":"added successfully"})
+# @csrf_exempt
+# def addTodo(request):
+#     if request.method == "POST":
+#         data = json.loads(request.body)
+#         title = data.get("title")
+#         desc = data.get('description')
+#         status = data.get('status')
+#         TodoList.objects.create(title=title,description=desc)
+#         return JsonResponse({"message":"added successfully"})
 
-def readTodo(request):
-    if request.method == "GET":
-        todolists=list(TodoList.objects.all().values())
+# def readTodo(request):
+#     if request.method == "GET":
+#         todolists=list(TodoList.objects.all().values())
         
-        return JsonResponse({"lists":todolists})
+#         return JsonResponse({"lists":todolists})
 
-def deleteTodo(request,id):
-    if request.method == "GET":
-        TodoList.objects.filter(id=id).delete()
-        return JsonResponse({"message":'Deleted Successfully'})
+# def deleteTodo(request,id):
+#     if request.method == "GET":
+#         TodoList.objects.filter(id=id).delete()
+#         return JsonResponse({"message":'Deleted Successfully'})
 
     
 
